00056726ad5d497e5c00a6cce9be4ca9ce71fbd6
Thanh Ngo <thanhngo26@hotmail.com>
2020-02-08 11:50:02 -0800

    firstReact assignment

---
+++ React/firstReact/script.js
@@ -0,0 +1,7 @@
+...
+<script>
+    const App = () => {
+        return React.createElement("h1", {}, "Our First React page has rendered");
+    };
+    ReactDOM.render(React.createElement(App), document.getElementById("root"));
+</script>
